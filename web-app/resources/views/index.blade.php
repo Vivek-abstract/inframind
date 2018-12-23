8a724e7308062972cf6f29bdfb6166b6e1c55e34
Vivek Gawande <vivek040997@gmail.com>
2018-12-23 20:44:01 +0530

    Create Web app default template

---
+++ web-app/resources/views/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master') 
+@section('hero-image')
+
+<header class="masthead">
+    <div class="jumbotron jumbotron-fluid vertical-center" style="background-image: url('/img/home-bg.jpg')">
+        <div class="overlay"></div>
+        <div class="container text-center dont-overlay">
+            <div class="inner-container">
+                <h1 class="display-4 heading">Technocratic Info Solutions</h1>
+                <p class="no-margin">We are the leading cloud architecture provider for your application</p>
+            </div>
+        </div>
+    </div>
+</header>
+@endsection
