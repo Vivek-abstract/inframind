2e110da64a7c3535f2d618914c0df475cd65ba29
Vivek Gawande <vivek040997@gmail.com>
2018-12-25 17:13:56 +0530

    Show message when no launch requests are present

--- synergy/includes/connect.inc.php
+++ synergy/includes/connect.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-$servername = "10.0.1.207";
+$servername = "10.0.1.15";
 $username = "groot";
 $password = "groot";
 $dbname = 'synergy';
--- web-app/resources/views/launch/index.blade.php
+++ web-app/resources/views/launch/index.blade.php
@@ -5,27 +5,33 @@
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <h4>Your Launch Requests</h4>
-            <div class="table-responsive mt-4">
-                <table id="mytable" class="table table-bordred table-striped">
-                    <thead>
-                        <th>App Name</th>
-                        <th>Requester Name</th>
-                        <th>Contact</th>
-                        <th>Status</th>
-                    </thead>
-                    <tbody>
-                        @foreach ($launchRequests as $launchRequest)
-                        <tr>
-                            <td>{{ $launchRequest->stack_name }}</td>
-                            <td>{{ $launchRequest->requester }}</td>
-                            <td>{{ $launchRequest->contact }}</td>
-                            <td><a href="/launch/{{ $launchRequest->id }}" class="link">Check Status</a></td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
+            <h1>Your Launch Requests</h1>
+            <hr>
+
+            @if (count($launchRequests))
+                <div class="table-responsive mt-4">
+                    <table id="mytable" class="table table-bordred table-striped">
+                        <thead>
+                            <th>App Name</th>
+                            <th>Requester Name</th>
+                            <th>Contact</th>
+                            <th>Status</th>
+                        </thead>
+                        <tbody>
+                            @foreach ($launchRequests as $launchRequest)
+                            <tr>
+                                <td>{{ $launchRequest->stack_name }}</td>
+                                <td>{{ $launchRequest->requester }}</td>
+                                <td>{{ $launchRequest->contact }}</td>
+                                <td><a href="/launch/{{ $launchRequest->id }}" class="link">Check Status</a></td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            @else
+                <p>No Launch Requests</p>
+            @endif
             <div class="text-center">
                 <a href='/launch/create' class="btn btn-primary text-center">Launch New App</a>
             </div>
